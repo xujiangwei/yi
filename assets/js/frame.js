8018e9bdb6c8e427d930caed8fc284a1a3d40db0
Jiangwei Xu <hermit86@163.com>
2013-10-30 11:08:19 +0800

    变更 lib 目录结构

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -100,7 +100,7 @@ window.yi = {
 	common.config({
 		base: "./lib/",
 		alias: {
-			"console": "console/console.js",
+			"console": "core/console/console.js",
 			"menu-aim": "plugins/jquery.menu-aim.js",
 			"dialog": "plugins/bootbox.min.js"
 		}
@@ -118,6 +118,29 @@ window.yi = {
 			app.main();
 		});
 	}
+
+	// 自动调整 Sidebar 高度
+	(function() {
+		var $sb = $('.sidebar-nav');
+		var ww = parseInt(window.innerWidth);
+		if (ww >= 992) {
+			var sbh = Math.max(parseInt($('.page-wrapper').height())
+				, parseInt(document.body.clientHeight) - 50);
+			$sb.height(sbh);
+		}
+
+		$(window).resize(function(e) {
+			var ww = parseInt(window.innerWidth);
+			if (ww >= 992) {
+				var sbh = Math.max(parseInt($('.page-wrapper').height())
+					, parseInt(document.body.clientHeight) - 50);
+				$sb.height(sbh);
+			}
+			else {
+				$sb.height('auto');
+			}
+        });
+	})();
 
 	// 框架配置
 	window.yi.setup();
