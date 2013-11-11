45d75eab4cc08bd121876bfd9facdfd0290f1a84
Jiangwei Xu <hermit86@163.com>
2013-11-11 14:54:19 +0800

    修改 yi.js 工程结构
    将 Yi 中与具体页面相关的代码提取到 frame.js 里，为后面的 mod 模块提供统一结构

---
+++ scripts/compile-yi.cmd
@@ -0,0 +1,11 @@
+@echo off
+rem Author Ambrose Xu (Xu Jiangwei)
+
+java -jar ../tools/splicer.jar ../lib/core/yi/yi.js ../lib/core/yi/src/intro.js ../lib/core/yi/src/base.js ../lib/core/yi/src/outro.js
+
+rem WHITESPACE_ONLY | SIMPLE_OPTIMIZATIONS | ADVANCED_OPTIMIZATIONS
+java -jar ../tools/compiler.jar --compilation_level WHITESPACE_ONLY --charset UTF-8 --js=../lib/core/yi/yi.js --js_output_file=../lib/core/yi/yi.min.js
+
+echo.
+echo Press any key.
+pause > nul
